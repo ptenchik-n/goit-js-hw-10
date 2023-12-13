8527c8e8f6f7b7c596442ae21b5482736cbbbbb0
Anastasiia <nastia.ptenchik11@gmail.com>
2023-12-13 14:27:47 +0300

    update

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,8 +1,10 @@
-const url = 'https://api.thecatapi.com/v1';
+import axios from "axios";
+axios.defaults.headers.common["x-api-key"] = "live_SvEKjafWJw5ap1Er2FAL9LB8W1zMm3EPFPrZp48RikiK9tqJV9WjNf8zmkC89NEm";
+const url_cat = 'https://api.thecatapi.com/v1';
 const api_key = 'live_SvEKjafWJw5ap1Er2FAL9LB8W1zMm3EPFPrZp48RikiK9tqJV9WjNf8zmkC89NEm';
 
 export function fetchBreeds() {
-  return fetch(`${url}/breeds?api_key=${api_key}`)
+  return fetch(`${url_cat}/breeds?api_key=${api_key}`)
     .then(response => {
         if(!response.ok) {
             throw new Error(response.status);
@@ -14,7 +16,7 @@ export function fetchBreeds() {
 
 export function fetchCatByBreed(breedId) {
   return fetch(
-    `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+    `${url_cat}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
     .then(response => {
         if(!response.ok)
         {throw new Error(response.status)}
